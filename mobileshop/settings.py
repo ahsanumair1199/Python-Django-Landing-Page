5daf4a64652ccf022991352e24d010801136e92e
ahsanumair1199 <93791080+ahsanumair1199@users.noreply.github.com>
2021-12-27 03:23:53 +0000

    Update settings.py

--- mobileshop/settings.py
+++ mobileshop/settings.py
@@ -136,7 +136,7 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ahsanumair1199@gmail.com'
-EMAIL_HOST_PASSWORD = 'ahsumrone1199'
+EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 EMAIL_USE_TLS = True
 django_heroku.settings(locals())
